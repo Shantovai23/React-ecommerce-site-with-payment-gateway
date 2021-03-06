2b1ed3539d5da35cd4fc11d81310dd6e83f5001f
Shantovai23 <hasibulhasan23sept@gmail.com>
2021-03-06 21:19:59 +0600

    navbar-and-header-created

---
+++ src/components/Banner.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Banner=()=>{
+    return(
+        <div className='container'>
+            <header>
+               <div className='headerText'>
+                  <div>
+                  <h1>Exclussive Collections</h1>
+                   <p>Enjoy Shopping With KenaKata.com</p>
+                  </div>
+               </div>
+           </header>
+        </div>
+    )
+}
+
+export default Banner
