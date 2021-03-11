2250159ac1d1b82af4ecdb898772e41562a471e0
Shantovai23 <hasibulhasan23sept@gmail.com>
2021-03-11 23:53:30 +0600

    responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav>
       <ul className='left'>
-        <li><Link to='/'>KenaKata.com</Link></li>
+        <li><Link to='/React-ecommerce-site-with-payment-gateway'>KenaKata.com</Link></li>
       </ul>
       <ul className='right'>
         <li><Link to='/cart'><span className='shoppingCart'><i className="fas fa-cart-plus"></i><span className='cartCount'>{qty}</span></span></Link></li>
